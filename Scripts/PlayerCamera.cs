6bdb74f0c1ac30445757e176d0fd464ab39dc37c
Parker <67876197+crowtime@users.noreply.github.com>
2025-11-17 19:29:51 -0500

    first person camera cursor movement added

---
+++ Scripts/PlayerCamera.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float x;
+    public float y;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * x * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * y * Time.deltaTime;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
