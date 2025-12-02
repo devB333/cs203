200c294bb01cef66458fbce00fb1edb257210194
Parker <67876197+crowtime@users.noreply.github.com>
2025-12-01 21:11:29 -0500

    added comments

--- Scripts/PortalTextureSetup.cs
+++ Scripts/PortalTextureSetup.cs
@@ -7,22 +7,25 @@ public class PortalTextureSetup : MonoBehaviour
     public Camera world1Cam;
     public Material world1CamMat;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // removes textures if they're already there
         if(world2Cam.targetTexture != null)
         {
             world2Cam.targetTexture.Release();
         }
+        // scales the portal texture based on the game display size
         world2Cam.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
         world2CamMat.mainTexture = world2Cam.targetTexture;
 
+        // removes textures if they're already there
         if(world1Cam.targetTexture != null)
         {
             world1Cam.targetTexture.Release();
         }
+        // scales the portal texture based on the game display size
         world1Cam.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
         world1CamMat.mainTexture = world1Cam.targetTexture;
     }
 }
+
